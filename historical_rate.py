091b5734c0bdac7a89ef29e26a594e6e447de3e3
galashov <igalashow@gmail.com>
2021-06-27 01:16:47 +0300

    Добавил инсерты, но они не работают

--- historical_rate.py
+++ historical_rate.py
@@ -1,6 +1,8 @@
 from datetime import date, timedelta
 import time
+import requests
 from bs4 import BeautifulSoup
+import pymysql
 
 def get_exchange_rate(source):
     """ Получает данные по курсам валют """
@@ -12,8 +14,15 @@ def get_value(valute_id):
     value = soup.find('valute', {'id' : valute_id}).find('value').get_text()
     return float(value.replace(',', '.'))
 
-def get_date(soup):
-    """ Получает дату """
+dwh = pymysql.connect(
+  host="localhost",
+  user="my_sql_user",
+  database="database_dwh", #database
+  passwd="qwerty"
+)
+
+mycursor = dwh.cursor()
+
 
 currencies_id = {
     'USD' : 'R01235',
@@ -21,8 +30,8 @@ currencies_id = {
     'CNY' : 'R01375',
     }
 url = 'https://www.cbr.ru/scripts/XML_daily.asp?date_req='
-histor_date = date.today()
 
+histor_date = date.today()
 for delta_day in range(30):
     historic_date = histor_date - timedelta(days=delta_day)
     str_date = historic_date.strftime('%d/%m/%Y')
@@ -32,7 +41,15 @@ for delta_day in range(30):
 
     soup = BeautifulSoup(raw_data, 'html.parser')
 
-    for id in currencies_id:
-        print(id, get_value(currencies_id[id]))
+    for ident in currencies_id:
+        value = get_value(currencies_id[ident])
+        print(ident, value, historic_date)
+        query = 'INSERT INTO `staging` VALUES (ident, "RUB", value, historic_date, "ЦБ РФ")'
+        mycursor.execute(query)
+        dwh.commit()
+    print('RUB', 1.0, historic_date)
+    query = 'INSERT INTO `staging` VALUES ("RUB", "RUB", 1, historic_date, "ЦБ РФ")'
+    mycursor.execute(query)
+    dwh.commit()
 
     # print(usd_float)
