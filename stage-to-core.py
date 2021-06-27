a2811d9b481d509ec56f6ff61936e9f92b5538af
galashov <igalashow@gmail.com>
2021-06-27 23:05:22 +0300

    Наладил перегрузку из Staging в Core

--- stage-to-core.py
+++ stage-to-core.py
@@ -1,12 +1,10 @@
-from datetime import date, timedelta
-import time
 import pymysql
 import db_auth
 
 
 def get_dict(table):
     """ Переносит справочники в словарь """
-    tuple = get_handbook(table)
+    tuple = get_table(table)
     dict_ = {}
     for item in tuple:
         dict_.update([(item[0], item[1:])])
@@ -39,13 +37,34 @@ def cleaning_table(table):
 
 
 @connect_to_db
-def get_handbook(table):
-    """ Select данных из справочника """
+def get_table(table):
+    """ Select всех данных из таблицы """
     query = 'SELECT * FROM '+ table
     return query
 
+@connect_to_db
+def insert_to_core(core):
+    """ Инсертит данные в таблицу Core """
+    query = 'INSERT INTO '+core+' VALUES (%s, %s, %s, %s, %s)'
+    return query
+
+
+
 # Наполняем справочники
 currencies = get_dict('currencies')
-
 source = get_dict('source')
+
+# Получаем данные из staging
+staging = get_table('staging')
+
+# Формируем Core
+for row in staging:
+    curr_id1 = currencies[row[0]]
+    curr_id2 = currencies[row[1]]
+    source_id = source[row[4]]
+    ex_rate = row[2]
+    insert_to_core('core', curr_id1[0], curr_id2[0], ex_rate, row[3], source_id)
+
+cleaning_table('staging')
+
 
